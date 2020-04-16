4d713d80c8d353abcec74081ef2a942883bb9acf
Anna <annacmackenzie89@gmail.com>
2020-04-16 16:13:13 -0400

    adding react-i18n

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,41 +1,42 @@
-import App from 'next/app'
+import App from 'next/app';
 
 const NextJSApp = ({ Component, pageProps, lang }) => {
-    return (
-        <>
-            <Component {...pageProps} lang={lang} />
-        </>
-    )
-}
+  // i18n = (lang || 'en');
+  return (
+    <>
+      <Component {...pageProps} lang={lang} />
+    </>
+  );
+};
 
 NextJSApp.getInitialProps = async ({ Component, ctx }) => {
-    let pageProps = {}
-    let lang = 'en'
-    if (ctx && ctx.req) {
-        const host = ctx.req.headers.host
-        switch (host) {
-            case 'headlessboost.ch:3000':
-                lang = 'ger'
-                break;
+  let pageProps = {};
+  let lang = 'en';
+  if (ctx && ctx.req) {
+    const host = ctx.req.headers.host;
+    switch (host) {
+      case 'headlessboost.ch:3000':
+        lang = 'ger';
+        break;
 
-            case 'headlessboost.fr:3000':
-                lang = 'fr'
-                break;
+      case 'headlessboost.fr:3000':
+        lang = 'fr';
+        break;
 
-            case 'headlessboost.com:3000':
-                lang = 'en'
-                break;
+      case 'headlessboost.com:3000':
+        lang = 'en';
+        break;
 
-            case 'headlessboost.br:3000':
-                lang = 'pt'
-                break;
-        }
+      case 'headlessboost.br:3000':
+        lang = 'pt';
+        break;
     }
-    if (Component.getInitialProps) {
-        pageProps = await Component.getInitialProps(ctx)
-    }
+  }
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
 
-    return { pageProps, lang }
-}
+  return { pageProps, lang };
+};
 
-export default NextJSApp
+export default NextJSApp;
